5990e2a7cb030ee8ce493949799ed94efe1fefdd
asedlack <asedlackcode@gmail.com>
2020-04-18 14:05:24 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# ReadmeGenerator
+#Project 
+   
+
+   #Description
+   
+
+   #Table of Contents
+   
+
+   #Installation
+   
+
+   #Usage
+   
+
+   #License
+   
+
+   #Contributing
+   
+
+   #Tests
+   
+
+   #Github Email
+   email@email
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const util = require('util');
 const inquirer = require('inquirer');
+const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile)
 
@@ -54,17 +55,53 @@ let promptUser = () => {
         
 ])};
 
-const generateREADme = (answers) => {
-    return fs.writeFile("README.md", answers(err)) 
-        if (err) throw err;
-        console.log("it worked");
-    };
+
+
+const generateREADme = (answers, email, avatar_url) => {
+    var str =  `#Project 
+   ${answers.Project}
+
+   #Description
+   ${answers.Description}
+
+   #Table of Contents
+   ${answers.TableofContens}
+
+   #Installation
+   ${answers.Installation}
+
+   #Usage
+   ${answers.Usage}
+
+   #License
+   ${answers.License}
+
+   #Contributing
+   ${answers.Contributing}
+
+   #Tests
+   ${answers.Tests}
+
+   #Github Email
+   ${answers.githubEmail}
+`
+    axios.get("https://api.github.com/users/" + answers.githubEmail)
+        .then(res => answers.githubEmail = (res.data.email))
+        .catch(err => console.log(err));  
+   
+
+   return str
+}
+
+// 2 things to do 
+// 1 finishi gerating readme
+// 2 include response of API call to readme generation (coiuld do this first)
 
 
 async function init() {
     try {
         const answers = await promptUser();
-        const readme = generateREADme(answers);
+        const readme = generateREADme(answers, email, "https://asdf.com/url/to/avatar");
         await writeFileAsync("README.md", readme)
         console.log("success")
     }   catch(err){
